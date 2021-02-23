58fbe4446b24de29f7e195ca1c338c2e61b16817
Zicong Wang <actonnwang@gmail.com>
2021-02-23 14:22:02 -0500

    Update injectSO4.c

--- lab2/apps/q5/injectSO4.c
+++ lab2/apps/q5/injectSO4.c
@@ -8,24 +8,30 @@ void main (int argc, char *argv[])
 {
   uint32 h_mem;            // Handle to the shared memory page
   sem_t s_procs_completed; // Semaphore to signal the original process that we're done
-  lock_t buff_lock;			    // Lock for the buffer
+  sem_struct *all_sems; 
   int i = 0;                // index for str[]
+  int num_SO4;
 
-  if (argc != ) { 
-    Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore> <handle_to_lock>\n"); 
+  if (argc != 4) { 
+    Printf("Usage: "); Printf(argv[0]); 
+    Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore> <handle_to_number of SO4> \n"); 
     Exit();
   }
-  int numSO4 = dstrtol(argv[1], NULL, 10); // the "10" means base 10
+
+  h_mem = dstrtol(argv[1], NULL, 10); // The "10" means base 10
   s_procs_completed = dstrtol(argv[2], NULL, 10);
- 
-  // Now print a message to show that everything worked
-  // Printf("Producer: This is one of the %d instances you created.  ", buffer1->numprocs);
-  // Printf("Producer: Missile code is: %c\n", buffer1->really_important_char);
-  // Printf("Producer: My PID is %d\n", getpid());
+  num_SO4 = dstrtol(argv[3], NULL, 10);
+
+   // Map shared memory page into this process's memory space
+  if ((all_sems = (sem_struct*) shmat(h_mem)) == NULL) {
+    Printf("Could not map the virtual address to the memory in "); Printf(argv[0]); Printf(", exiting...\n");
+    Exit();
+  }
 
   // Signal the semaphore to tell the original process that we're done
-  while (i < numSO4){
+  while (i < num_SO4){
     Printf("SO4 injected into Radeon atmosphere, PID %d\n", getpid());
+    all_sems->numSO4 ++; 
     i++;
   }
 
