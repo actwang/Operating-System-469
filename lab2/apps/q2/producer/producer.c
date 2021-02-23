48c1bfa42c00cb1113500db5329ebfe3f13d4935
Zicong Wang <actonnwang@gmail.com>
2021-02-23 11:54:00 -0500

    Update producer.c

--- lab2/apps/q2/producer/producer.c
+++ lab2/apps/q2/producer/producer.c
@@ -6,9 +6,9 @@
 
 void main (int argc, char *argv[])
 {
-	uint32 h_mem;				      // Handle to the shared memory page
-	sem_t s_procs_completed;	// Semaphore to signal the original process that we're done
-
+  //missile_code *mc;        // Used to access missile codes in shared memory page
+  uint32 h_mem;            // Handle to the shared memory page
+  sem_t s_procs_completed; // Semaphore to signal the original process that we're done
   circ_buffer *buffer1;     // Used to get address of shared memory page
   lock_t buff_lock;			    // Lock for the buffer
 	int i = 0;                // index for str[]
@@ -16,41 +16,35 @@ void main (int argc, char *argv[])
   char str[] = "Hello World";
 	int length = dstrlen(str);
 
-  // if (argc != 3) {
-  //   Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore>\n");
-  //   Exit();
-  // }
+  if (argc != 4) { 
+    Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore> <handle_to_lock>\n"); 
+    Exit();
+  } 
 
-	if (argc != 4) {
-		Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore> <handle_to_lock>\n");
-		Exit();
-	}
-
-	// Convert the command-line strings into integers for use as handles
-	h_mem = dstrtol(argv[1], NULL, 10); 				// The "10" means base 10
-	s_procs_completed = dstrtol(argv[2], NULL, 10);		// Paged mapped semaphore
-
+  // Convert the command-line strings into integers for use as handles
+  h_mem = dstrtol(argv[1], NULL, 10); // The "10" means base 10
+  s_procs_completed = dstrtol(argv[2], NULL, 10);
   buff_lock = dstrtol(argv[3], NULL, 10);
 
-	// Map shared memory page into this process's memory space
-	if ((buffer1 = (circ_buffer *)shmat(h_mem)) == NULL) {
-		Printf("Could not map the virtual address to the memory in "); Printf(argv[0]); Printf(", exiting...\n");
-		Exit();
-	}
+  // Map shared memory page into this process's memory space
+  if ((buffer1 = (circ_buffer*) shmat(h_mem)) == NULL) {
+    Printf("Could not map the virtual address to the memory in "); Printf(argv[0]); Printf(", exiting...\n");
+    Exit();
+  }
 
-	while(i < length) {
+  while(i < length) {
 		if(lock_acquire(buff_lock) != SYNC_SUCCESS) {
 			Printf("Get buffer lock failed.\n");
 			Exit();
 		}
 
-		if (((buffer1->head + 1) % BUFFERSIZE) != buffer1->tail) {
-			// Buffer is not full
-			Printf("Producer %d inserted: %c\n", getpid(), str[i]);
-			buffer1->buffer[buffer1->head] = str[i];
-			buffer1->head = (buffer1->head + 1) % BUFFERSIZE;
-			i++;
-		}
+    if (((buffer1->tail + 1) % BUFFERSIZE) != buffer1->head) {
+      Printf("Producer %d inserted: %c\n", getpid(), str[i]);
+      buffer1->buffer[buffer1->tail % BUFFERSIZE] = str[i];
+      buffer1->tail = (buffer1->tail + 1) % BUFFERSIZE;
+      i++;
+    }
+
 
 		// Release the lock
 		if (lock_release(buff_lock) != SYNC_SUCCESS) {
@@ -58,11 +52,16 @@ void main (int argc, char *argv[])
 			Exit();
 		}
 	}
+ 
+  // Now print a message to show that everything worked
+  // Printf("Producer: This is one of the %d instances you created.  ", buffer1->numprocs);
+  // Printf("Producer: Missile code is: %c\n", buffer1->really_important_char);
+  // Printf("Producer: My PID is %d\n", getpid());
 
-	// Signal the semaphore to tell the original process that we're done
-	Printf("producer: PID %d is complete.\n", getpid());
-	if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
-		Printf("Bad semaphore s_procs_completed (%d) in ", s_procs_completed); Printf(argv[0]); Printf(", exiting...\n");
-		Exit();
-	}
+  // Signal the semaphore to tell the original process that we're done
+  Printf("Producer: PID %d is complete.\n", getpid());
+  if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("Bad semaphore s_procs_completed (%d) in ", s_procs_completed); Printf(argv[0]); Printf(", exiting...\n");
+    Exit();
+  }
 }
