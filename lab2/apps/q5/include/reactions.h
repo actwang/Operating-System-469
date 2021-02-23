4a88c19aa15ac6622433e70f96289eb7523c08b9
Zicong Wang <actonnwang@gmail.com>
2021-02-23 12:10:52 -0500

    Create reactions.h

---
+++ lab2/apps/q5/include/reactions.h
@@ -0,0 +1,23 @@
+#ifndef __USERPROG__
+#define __USERPROG__
+
+
+typedef struct _all_sems{
+    int numH2O;
+    int numSO4;
+    int numReact1;
+    int numReact2;
+    int numReact3;
+    int numH2;
+    int numO2;
+    int numSO2;
+    int numH2SO4;
+}all_sems;
+
+#define FILE_H2O "injectH2O.dlx.obj"
+#define FILE_SO4 "injectSO4.dlx.obj"
+#define FILE_REACT1 "react1.dlx.obj"
+#define FILE_REACT2 "react2.dlx.obj"
+#define FILE_REACT3 "react3.dlx.obj"
+
+#endif
