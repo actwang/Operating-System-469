dee2e08d4e697d16eb9e5f99a90830617126b96e
Zicong Wang <actonnwang@gmail.com>
2021-03-18 23:39:07 -0400

    Update injectS2.c

--- lab3/apps/q2/injectS2/injectS2.c
+++ lab3/apps/q2/injectS2/injectS2.c
@@ -1,32 +1,25 @@
 //#include "lab2-api.h"
 #include "usertraps.h"
 #include "misc.h"
-#include "mbox.h"
+//#include "mbox.h"
 
 #include "reactions.h"
 
 void main (int argc, char *argv[])
 {
-  //unsigned int h_mem;            // Handle to the shared memory page
   mbox_t S2_handle;
   sem_t s_procs_completed; // Semaphore to signal the original process that we're done
   mole_struct *molecules; 
-  int num_H2O;
 
   if (argc != 3) { 
     Printf("Usage: "); Printf(argv[0]); 
-    Printf(" <handle_to_mbox_S2> <handle_to_page_mapped_semaphore> \n"); 
+    Printf(" <handle_to_mbox_S2> <handle_to_process_semaphore> \n"); 
     Exit();
   }
 
-  S2_handle = dstrtol(argv[1], NULL, 10); // Do we still use shmat??? or just pass handle as argument?
+  S2_handle = dstrtol(argv[1], NULL, 10);
   s_procs_completed = dstrtol(argv[2], NULL, 10);
 
-//    // Map shared memory page into this process's memory space
-//   if ((molecules = (mole_struct*) shmat(h_mem)) == NULL) {
-//     Printf("Could not map the virtual address to the memory in "); Printf(argv[0]); Printf(", exiting...\n");
-//     Exit();
-//   }
 
   if (mbox_open(S2_handle) != MBOX_SUCCESS){
     Printf("Open mbox failed in injecting S2, PID %d\n", getpid());
