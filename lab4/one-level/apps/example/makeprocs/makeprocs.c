004a08cbc7e774e4884237244cfade4c6324f3dd
Zicong Wang <actonnwang@gmail.com>
2021-04-19 18:05:09 -0400

    Update makeprocs.c

--- lab4/one-level/apps/example/makeprocs/makeprocs.c
+++ lab4/one-level/apps/example/makeprocs/makeprocs.c
@@ -11,7 +11,7 @@
 
 void main (int argc, char *argv[])
 {
-  int test_num = 0;             // Used to store number of processes to create
+  int test_num;             // Used to store number of processes to create
   int i,j;                               // Loop index variable
   sem_t s_procs;             // Semaphore used to wait until all spawned processes have completed
   char s_procs_str[10];      // Used as command-line argument to pass page_mapped handle to new processes
@@ -23,8 +23,9 @@ void main (int argc, char *argv[])
 
   // Convert string from ascii command line argument to integer number
   test_num = dstrtol(argv[1], NULL, 10); // the "10" means base 10
-  if (!(test_num >= 1 && test_num <= 6)){
+  if (test_num < 1 || test_num > 6){
     Printf("Invalid test number. Enter a number between 1 and 6\n");
+    Exit();
   }
   Printf("makeprocs (%d): Creating %d test processes\n", getpid(), test_num);
 
@@ -32,32 +33,37 @@ void main (int argc, char *argv[])
   // Create processes
   Printf("-------------------------------------------------------------------------------------\n");
 
-  for(i=0; i < test_num; i++) {
+  for(i=1; i < 7; i++) {
     if (i == 1){
+      Printf("Running Test # 1\n");
       s_procs = sem_create(0);
       ditoa(s_procs,s_procs_str);
       process_create(Q2_1, s_procs_str, NULL);
       sem_wait(s_procs);
     }
     else if (i == 2){
+      Printf("Running Test # 2\n");
       s_procs = sem_create(0);
       ditoa(s_procs, s_procs_str);
       process_create(Q2_2, s_procs_str, NULL);
       sem_wait(s_procs);
     }
     else if (i == 3){
+      Printf("Running Test # 3\n");
       s_procs = sem_create(0);
       ditoa(s_procs, s_procs_str);
       process_create(Q2_3, s_procs_str, NULL);
       sem_wait(s_procs);
     }
     else if (i == 4){
+      Printf("Running Test # 4\n");
       s_procs = sem_create(0);
       ditoa(s_procs, s_procs_str);
       process_create(Q2_4, s_procs_str, NULL);
       sem_wait(s_procs);
     }
     else if (i == 5){
+      Printf("Running Test # 5\n");
       s_procs = sem_create(1-100);
       ditoa(s_procs, s_procs_str);
       for (j = 0; j < 100; j++){
@@ -66,12 +72,15 @@ void main (int argc, char *argv[])
       sem_wait(s_procs);
     }
     else if (i == 6){
+      Printf("Running Test # 6\n");
       s_procs = sem_create(1-30);
       ditoa(s_procs, s_procs_str);
+      Printf("REACHED\n");
       for (j = 0; j < 30; j++){
         process_create(Q2_6, s_procs_str, NULL);
       }
       sem_wait(s_procs);
+      Printf("REACHED\n");
     }
     else {
       Printf("Invalid test number\n");
