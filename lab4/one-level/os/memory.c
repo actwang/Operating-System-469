b297a840870dc46c278ca95fdef4f27f29ac8dd3
actwang <actonnwang@gmail.com>
2021-04-23 15:01:28 -0400

    mem < for fork and onelevel

--- lab4/one-level/os/memory.c
+++ lab4/one-level/os/memory.c
@@ -218,8 +218,8 @@ int MemoryPageFaultHandler(PCB *pcb) {
   // Page number for user stack
   userStack_pagenum = (pcb->currentSavedFrame[PROCESS_STACK_USER_STACKPOINTER] - PROCESS_STACK_FRAME_SIZE) >> MEM_L1FIELD_FIRST_BITNUM;
   
-  if (fault_pagenum > userStack_pagenum){
-    dbprintf('m',"SegFault(fault address higher than user stack pointer) in Memory Page Fault Handler.\n");
+  if (fault_pagenum < userStack_pagenum){
+    printf("SegFault(fault address higher than user stack pointer) in Memory Page Fault Handler.\n");
     ProcessKill();
     return MEM_FAIL;
   }
