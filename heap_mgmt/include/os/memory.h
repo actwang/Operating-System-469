4dccb472412d2dfc574f0db0860f3268784df82b
Zicong Wang <actonnwang@gmail.com>
2021-04-24 21:17:24 -0400

    Add files via upload

---
+++ heap_mgmt/include/os/memory.h
@@ -0,0 +1,38 @@
+#ifndef	_memory_h_
+#define	_memory_h_
+
+// Put all your #define's in memory_constants.h
+#include "memory_constants.h"
+
+extern int lastosaddress; // Defined in an assembly file
+
+//--------------------------------------------------------
+// Existing function prototypes:
+//--------------------------------------------------------
+
+int MemoryGetSize();
+void MemoryModuleInit();
+uint32 MemoryTranslateUserToSystem (PCB *pcb, uint32 addr);
+int MemoryMoveBetweenSpaces (PCB *pcb, unsigned char *system, unsigned char *user, int n, int dir);
+int MemoryCopySystemToUser (PCB *pcb, unsigned char *from, unsigned char *to, int n);
+int MemoryCopyUserToSystem (PCB *pcb, unsigned char *from, unsigned char *to, int n);
+int MemoryPageFaultHandler(PCB *pcb);
+
+//---------------------------------------------------------
+// Put your function prototypes here
+//---------------------------------------------------------
+// All function prototypes including the malloc and mfree functions go here
+uint32 MemorySetupPte (uint32 page) ;
+void MemoryFreePage(uint32 page) ;
+int MemoryAllocPage(void);
+
+
+//void *malloc();
+void* malloc(PCB* pcb, int memsize);
+//int mfree();
+int mfree(PCB* pcb, void* ptr);
+
+int MallocNodeHelper(heapNode* node, PCB* pcb, int memsize);
+void MemoryMergeNodes(heapNode* Hnode, PCB *pcb);
+
+#endif	// _memory_h_
